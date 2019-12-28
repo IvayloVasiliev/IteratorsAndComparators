ab026c997cfea8be6d3356dbf705730c19979e16
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2019-12-28 22:34:26 +0200

    Problem 6.  Strategy Pattern

---
+++ StrategyPattern06/PersonByAge.cs
@@ -0,0 +1,14 @@
+﻿namespace StrategyPattern06
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    public class PersonByAge : IComparer<Person>
+    {
+        public int Compare(Person first, Person second)
+        {
+            return first.Age.CompareTo(second.Age);
+
+        }
+    }
+}
---
+++ StrategyPattern06/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace StrategyPattern06
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<Person> peopleByName = new SortedSet<Person>(new PersonByName());
+            SortedSet<Person> peopleByAge = new SortedSet<Person>(new PersonByAge());
+
+            while (n-- > 0)
+            {
+                string[] inputArgs = Console.ReadLine().Split();
+
+                Person person = new Person(inputArgs[0], int.Parse(inputArgs[1]));
+
+                peopleByName.Add(person);
+                peopleByAge.Add(person);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, peopleByName));
+            Console.WriteLine(string.Join(Environment.NewLine, peopleByAge));
+
+        }
+    }
+}
