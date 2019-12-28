674b5d279cadc695df29221b98eabf29b023eb31
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2019-12-28 16:15:29 +0200

    Add project files.

---
+++ ListyIterator01/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace ListyIterator01
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            string[] data = Console.ReadLine().Split().Skip(1).ToArray();
+
+            ListyIterator<string> list = new ListyIterator<string>(data);
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                switch (input)
+                {
+                    case "Move":
+                        Console.WriteLine(list.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(list.HasNext());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            list.Print();
+                        }
+                        catch (InvalidOperationException e)
+                        {
+                            Console.WriteLine(e.Message); ;
+                        }
+                        break;
+
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
