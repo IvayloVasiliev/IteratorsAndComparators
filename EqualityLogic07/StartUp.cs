6924c95f0ab76094ceb33ff3b32b8f918d06f07c
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2019-12-28 22:59:10 +0200

    Problem 7. *Equality Logic

---
+++ EqualityLogic07/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace EqualityLogic07
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<Person> sortedPeople = new SortedSet<Person>();
+            HashSet<Person> peopleByHash = new HashSet<Person>();
+
+            while (n-- > 0)
+            {
+                string[] inputArgs = Console.ReadLine().Split();
+
+                Person person = new Person(inputArgs[0], int.Parse(inputArgs[1]));
+
+                sortedPeople.Add(person);
+                peopleByHash.Add(person);
+            }
+
+            Console.WriteLine(sortedPeople.Count);
+            Console.WriteLine(peopleByHash.Count);
+
+        }
+    }
+}
